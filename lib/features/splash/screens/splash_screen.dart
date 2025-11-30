aa720bd9d236151fd0bc2850a68f5c16527e10b8
Hoda Elkayal <helkayal169@gmail.com>
2025-12-01 00:53:17 +0200

    splash screen

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -3,26 +3,42 @@ import 'package:task9_quiz_app/core/theme/app_colors.dart';
 import 'package:task9_quiz_app/core/utils/app_string.dart';
 import 'package:task9_quiz_app/features/home/screens/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Center(
         child: Column(
+          spacing: 40,
           mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 20,
           children: [
-            InkWell(
-              onTap: () => Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (_) => const HomeScreen()),
-              ),
-              child: Text(
-                AppString.appTitle,
-                style: TextStyle(fontSize: 24, color: AppColors.whiteColor),
-              ),
+            Image.asset(
+              'assets/images/quiz_logo.png',
+              width: screenWidth * 0.5,
+            ),
+            Text(
+              AppString.appTitle,
+              style: TextStyle(fontSize: 24, color: AppColors.whiteColor),
             ),
             CircularProgressIndicator(color: AppColors.whiteColor),
           ],
